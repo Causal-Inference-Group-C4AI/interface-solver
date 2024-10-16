1f5c1e51f1be43c68fc8308537e1fdb4f06a0522
lucas <lucas-marques2003@usp.br>
2024-10-16 15:55:20 -0300

    Add base dowhy solver

--- src/solver_interfaces/dowhy_interface.py
+++ src/solver_interfaces/dowhy_interface.py
@@ -1,4 +1,27 @@
-import dowhy
+from dowhy import CausalModel
 
-def dowhy_solver():
-    pass
+
+def dowhy_solver(self):
+    # Step 1: Model
+    model = CausalModel(
+        data=self.data,
+        treatment="X",
+        outcome="Y",
+        graph=self.graph
+    )
+
+    # Step 2: Identify
+    identified_estimand = model.identify_effect()
+
+    # Step 3: Estimate
+    estimate = model.estimate_effect(
+        identified_estimand,
+        method_name="backdoor.linear_regression"
+    )
+
+    # Step 4: Refute
+    refutation = model.refute_estimate(
+        identified_estimand,
+        estimate,
+        method_name="random_common_cause"
+    )
