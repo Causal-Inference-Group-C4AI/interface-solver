01466dc634979bfeafcdf0b2fae2bdb55cf96c4d
Daniel Lawand <daniel.lawand@gmail.com>
2024-10-17 18:03:38 -0300

    WIP bcause solver

--- src/solver_interfaces/bcause_interface.py
+++ src/solver_interfaces/bcause_interface.py
@@ -1,10 +1,15 @@
 from bcause.inference.causal.multi import EMCC
 from bcause.models.cmodel import StructuralCausalModel
 import pandas as pd
+import bcause as bc
 
 def bcause_solver(uai_path, csv_path):
     model = StructuralCausalModel.read(uai_path)
-    dataset =  pd.read_csv(csv_path)
+    # TODO: esse UAI com o csv não tá funcionando
+    # dataset =  pd.read_csv(csv_path)
+    bc.randomUtil.seed(1)
+    dataset = model.sampleEndogenous(1000)
+
     inf = EMCC(model, dataset, max_iter=100, num_runs=20)
     cause, effect = [model.endogenous[i] for i in [0, -1]]
 
