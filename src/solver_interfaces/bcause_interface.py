f16857794b40acd466868a39b9d00b6ddc253786
Daniel Lawand <daniel.lawand@gmail.com>
2024-10-18 16:44:39 -0300

    Add runable bcause solver

--- src/solver_interfaces/bcause_interface.py
+++ src/solver_interfaces/bcause_interface.py
@@ -3,32 +3,20 @@ from bcause.models.cmodel import StructuralCausalModel
 import pandas as pd
 import bcause as bc
 
-def bcause_solver(uai_path, csv_path):
+def bcause_solver(uai_path: str, csv_path: str, treatment_variable: str, outcome_variable: str):
+    # Quando se lê do .UAI as variáveis são nomeadas como Vi, 0 <= i <= n-1, n é o número de nós que tem no grafo.
     model = StructuralCausalModel.read(uai_path)
-    # TODO: esse UAI com o csv não tá funcionando
-    # dataset =  pd.read_csv(csv_path)
-    bc.randomUtil.seed(1)
-    dataset = model.sampleEndogenous(1000)
 
-    inf = EMCC(model, dataset, max_iter=100, num_runs=20)
-    cause, effect = [model.endogenous[i] for i in [0, -1]]
+    mapping = {"V0":"Z", "V1":"X", "V2": "Y"}
+    renamed_model = model.rename_vars(mapping)
+    
+    dataset = pd.read_csv(csv_path)
+    inf = EMCC(renamed_model, dataset, max_iter=100, num_runs=20)
 
-    X, Y = cause, effect
+    X, Y = treatment_variable, outcome_variable
     p_do0 = inf.causal_query(Y, do={X: 0})
     p_do1 = inf.causal_query(Y, do={X: 1})
-    # actual = p.values
 
-    # The lowest prob Y = 0 | do(T=0)
-    # p.values[0]
-
-    # The lowest prob Y = 1 | do(T=0)
-    # p.values[1]
-
-    # The highest prob Y = 0 | do(T=0)
-    # p.values[2]
-
-    # The highest prob Y = 1 | do(T=0)
-    # p.values[3]
     print(
         f'P(OUTCOME=1|do(Treatment=0)) = {[p_do0.values[1], p_do0.values[3]]}')
     print(
