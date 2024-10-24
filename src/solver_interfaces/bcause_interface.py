d79c96f69bdb3dab96e8ece3108a88de715875bf
Daniel Lawand <daniel.lawand@gmail.com>
2024-10-24 10:30:09 -0300

    Update variable names

--- src/solver_interfaces/bcause_interface.py
+++ src/solver_interfaces/bcause_interface.py
@@ -20,23 +20,22 @@ def write_output(output, output_path="outputs/bcause_output_NO_NAME.txt", new=Fa
         f.write(output + "\n")
 
 
-def bcause_solver(test_name: str, uai_path: str, csv_path: str, treatment_variable: str, outcome_variable: str, mapping: dict):
+def bcause_solver(test_name: str, uai_path: str, csv_path: str, treatment: str, outcome: str, mapping: dict):
     model = StructuralCausalModel.read(uai_path)
     renamed_model = model.rename_vars(mapping)
 
     dataset = pd.read_csv(csv_path)
     inf = EMCC(renamed_model, dataset, max_iter=100, num_runs=20)
 
-    X, Y = treatment_variable, outcome_variable
-    p_do0 = inf.causal_query(Y, do={X: 0})
-    p_do1 = inf.causal_query(Y, do={X: 1})
+    p_do0 = inf.causal_query(outcome, do={treatment: 0})
+    p_do1 = inf.causal_query(outcome, do={treatment: 1})
 
     # Extracting bounds
     lower_bound = p_do1.values[1] - p_do0.values[1]
     upper_bound = p_do1.values[3] - p_do0.values[3]
     output_file_path = f"outputs/{test_name}/bcause_{test_name}.txt"
     write_output("==============================================", output_path=output_file_path, new=True)
-    write_output(f'P({outcome_variable}=1|do({treatment_variable}=0)) = {[p_do0.values[1], p_do0.values[3]]}', output_path=output_file_path)
-    write_output(f'P({outcome_variable}=1|do({treatment_variable}=1)) = {[p_do1.values[1], p_do1.values[3]]}', output_path=output_file_path)
+    write_output(f'P({outcome}=1|do({treatment}=0)) = {[p_do0.values[1], p_do0.values[3]]}', output_path=output_file_path)
+    write_output(f'P({outcome}=1|do({treatment}=1)) = {[p_do1.values[1], p_do1.values[3]]}', output_path=output_file_path)
     write_output(f"Causal effect lies in the interval [{lower_bound}, {upper_bound}]", output_path=output_file_path)
     write_output("==============================================", output_path=output_file_path)
