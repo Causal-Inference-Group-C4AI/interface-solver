5a1f475d56699c041938c13a35de56d55f953092
Daniel Lawand <daniel.lawand@gmail.com>
2024-10-31 18:18:29 -0300

    Data gen update

--- utils/dataGen.py
+++ utils/dataGen.py
@@ -1,10 +1,15 @@
 import numpy as np
+import pandas as pd
+import argparse
+import json
+from pathlib import Path
+
 from generator import generateMechanisms
 from graph import Graph
 from probabilityGen import distribution
 
 
-def dataGen(graph: Graph, numSamp=int(1e3)):
+def dataGen(graph: Graph, numSamp=int(1e3), csv_path='./data/csv/unobDataGenOutput.csv'):
 
     distExogen: list[np.array] = distribution(graph)
     truthTable: list[list[np.array]] = generateMechanisms(graph)
@@ -34,13 +39,34 @@ def dataGen(graph: Graph, numSamp=int(1e3)):
             return None
 
         experiment.append(vals)
+    
+    experiment = [[element - 1 for element in row] for row in experiment]
+    df = pd.DataFrame(experiment)
+
+    df_label = []
+    for i in range(1, graph.num_nodes+1):   
+        df_label.append(graph.index_to_label[i])
+
+    df.columns = df_label
+    df = df.drop(columns=["U"])
+    df.to_csv(csv_path, index=False)
+
     return experiment
 
 
 if __name__ == "__main__":
-    graph: Graph = Graph.parse()
+    parser = argparse.ArgumentParser(
+        description="Generates csv from a giving DAG.")
+    parser.add_argument('file_path',
+                        help='The path to the file you want to read')
+    args = parser.parse_args()
+
+    graph: Graph = Graph.parse(input_path=args.file_path)
+    # folder_name = Path(f"outputs/{test['test_name']}")
+    # folder_name.mkdir(parents=True, exist_ok=True)
+
     for i in range(1, graph.num_nodes + 1):
         if graph.cardinalities[i] < 1:
             graph.cardinalities[i] = 2
 
-    print(dataGen(graph=graph, numSamp=5))
+    print(dataGen(graph=graph, numSamp=1000))
