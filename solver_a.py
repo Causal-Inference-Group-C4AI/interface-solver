eb0e78920f1eadeea8816c45a3daf271db56321c
Daniel Lawand <daniel.lawand@gmail.com>
2024-11-19 18:44:57 -0300

    Remove unused files

--- solver_a.py
+++
@@ -1,7 +0,0 @@
-def solver_a(**kwargs):
-    # Pulling the dictionary from XCom (from the get_data task)
-    data = kwargs['ti'].xcom_pull(task_ids='get_data', key='data_dict')
-    
-    # Now you can use the data in solver_a
-    print(f"Data received in solver_a: {data}")
-
