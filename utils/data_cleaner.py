498502f8452e3b45021584cce944f1518ba8a43b
Davi Coelho <davigc1942@gmail.com>
2024-11-26 15:53:51 -0300

    Refactor cleanup_log e cleanup_lcn
    Juntando as duas funções similares em uma só, que deleta todos os arquivos com uma extensão específica em um diretório específico.

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -12,16 +12,20 @@ class DataCleaner:
         except Exception as e:
             raise Exception(f"Error deleting {file_path}: {e}")
 
+    def cleanup_extension(self, extension: str):
+        files = glob.glob(f"*.{extension}")
+        for file in files:
+            self.cleanup_file(file)
 
-    def cleanup_lcn(self):
-        """Remove all LCN files in the current directory."""
-        lcn_files = glob.glob("*.lcn")
-        for lcn_file in lcn_files:
-            self.cleanup_file(lcn_file)
+    # def cleanup_lcn(self):
+    #     """Remove all LCN files in the current directory."""
+    #     lcn_files = glob.glob("*.lcn")
+    #     for lcn_file in lcn_files:
+    #         self.cleanup_file(lcn_file)
 
 
-    def cleanup_logs(self):
-        """Remove all log files in the current directory."""
-        log_files = glob.glob(".*.log")
-        for log_file in log_files:
-            self.cleanup_file(log_file)
+    # def cleanup_logs(self):
+    #     """Remove all log files in the current directory."""
+    #     log_files = glob.glob(".*.log")
+    #     for log_file in log_files:
+    #         self.cleanup_file(log_file)
