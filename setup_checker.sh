f7befd6e50ec516adfb872683a23ce32cb7687bf
lucasmarques2003 <lucas-marques2003@usp.br>
2024-12-03 19:31:01 -0300

    Improve README formatting and add setup checker script

---
+++ setup_checker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run all setup scripts
+source setup_autobounds.sh
+source setup_bcause.sh
+source setup_dowhy.sh
+source setup_lcn.sh
+
+echo "All setup scripts have been executed successfully."
